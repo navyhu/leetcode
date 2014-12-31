efb6ca98c08a5f7a7e74cf0fc2c097d2799177b3
administrator <hahu@localhost.com>
2014-12-31 06:59:09 -0500

    Add a generater to generate random int array

--- src/com/leetcode/test/RandomGenerater.java
+++ src/com/leetcode/test/RandomGenerater.java
@@ -4,13 +4,28 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class RandomGenerater {
-	public static Integer[] generateRaondomIntArray(int n, int upper) {
-		//ArrayList<Integer> intArray = new ArrayList<Integer>();
+	public static Integer[] generateRandomIntegerArray(int n, int upper) {
+		// ArrayList<Integer> intArray = new ArrayList<Integer>();
 		Integer[] intArray = new Integer[n];
 		Random random = new Random();
-		
-		for (int i = 0;i < n;i++) {
+
+		for (int i = 0; i < n; i++) {
 			Integer randInt = random.nextInt(upper);
+			intArray[i] = randInt;
+			System.out.print(randInt + " ");
+		}
+		System.out.println("");
+
+		return intArray;
+	}
+
+	public static int[] generateRandomIntArray(int n, int upper) {
+		// ArrayList<Integer> intArray = new ArrayList<Integer>();
+		int[] intArray = new int[n];
+		Random random = new Random();
+
+		for (int i = 0; i < n; i++) {
+			int randInt = random.nextInt(upper);
 			intArray[i] = randInt;
 			System.out.print(randInt + " ");
 		}
